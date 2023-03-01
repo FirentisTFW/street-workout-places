84ec0ce329bfd5853bd0389065590a427d97f63e
FirentisTFW <jkb.pawel@gmail.com>
2023-03-01 18:33:50 +0100

    Add custom dropdown menu

---
+++ lib/widgets/dropdown_menu/dropdown_item_style.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DropdownItemStyle {
+  final BorderRadius? borderRadius;
+  final Color? color;
+  final BoxConstraints? constraints;
+  final Offset? offset;
+  final EdgeInsets? padding;
+
+  const DropdownItemStyle({
+    this.borderRadius,
+    this.color,
+    this.constraints,
+    this.offset,
+    this.padding,
+  });
+}
