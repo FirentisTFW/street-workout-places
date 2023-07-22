ad2f7c2fd40cbbeb306989b1cb8a5f1f91fcb7b9
FirentisTFW <jkb.pawel@gmail.com>
2023-07-22 22:08:11 +0200

    Move files to proper directory

--- lib/presentation/pages/home/home_widget_factory.dart
+++ lib/presentation/pages/home/home_widget_factory.dart
@@ -1,8 +1,8 @@
 import 'package:app/domain/services/user_location_service.dart';
 import 'package:app/presentation/pages/home/home_section.dart';
-import 'package:app/presentation/pages/home/widgets/cubit/spots_closest_to_user_cubit.dart';
 import 'package:app/presentation/pages/home/widgets/recently_added_spots_section.dart';
-import 'package:app/presentation/pages/home/widgets/spots_closest_to_user_section.dart';
+import 'package:app/presentation/pages/home/widgets/spots_closest_to_user/cubit/spots_closest_to_user_cubit.dart';
+import 'package:app/presentation/pages/home/widgets/spots_closest_to_user/spots_closest_to_user_section.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
