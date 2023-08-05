b5279dc08c3f736733d23040c45310786059599e
FirentisTFW <jkb.pawel@gmail.com>
2023-08-05 17:01:21 +0200

    Make home page map points clickable - go to spots details page

--- lib/presentation/pages/home/widgets/spots_closest_to_user/spots_closest_to_user_map.dart
+++ lib/presentation/pages/home/widgets/spots_closest_to_user/spots_closest_to_user_map.dart
@@ -1,8 +1,12 @@
 import 'package:app/domain/core/common/constants.dart';
 import 'package:app/domain/core/common/maps/map_coordinator.dart';
+import 'package:app/domain/core/common/root_navigator.dart';
 import 'package:app/domain/core/mappers/mappers.dart';
 import 'package:app/domain/models/map_essentials.dart';
+import 'package:app/domain/models/workout_spot_model.dart';
 import 'package:app/presentation/pages/home/widgets/spots_closest_to_user/cubit/spots_closest_to_user_cubit.dart';
+import 'package:app/presentation/pages/spot_details/spot_details_arguments.dart';
+import 'package:app/presentation/routing/dashboard_tabs/spots_routing.dart';
 import 'package:flutter/material.dart';
 
 class SpotsClosestToUserMap extends StatelessWidget {
@@ -26,6 +30,14 @@ class SpotsClosestToUserMap extends StatelessWidget {
         minZoom: Constants.maps.minLocationZoom,
         zoom: Constants.maps.defaultPositionDirectZoom,
       ),
+      onSpotPressed: (spot) => _goToSpotDetails(context, spot),
     );
   }
+
+  void _goToSpotDetails(BuildContext context, WorkoutSpotModel spot) => RootNavigator.of(context).pushNamed(
+        SpotsRouting.spotDetails,
+        arguments: SpotDetailsArguments(
+          spot: spot,
+        ),
+      );
 }
